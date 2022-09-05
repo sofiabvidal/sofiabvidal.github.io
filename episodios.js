3a3e48a555fb942fdf550aa24d6ff2a58865c578
sofiabvidal <joaoaugustolanjoni@gmail.com>
2022-09-05 20:28:55 -0300

    Update

--- episodios.js
+++ episodios.js
@@ -35,18 +35,3 @@ $(document).ready(function() {
         return false;
     });
 });
-function leiaMais() {
-    var pontos = document.getElementById("pontos");
-    var maisTexto = document.getElementById("mais");
-    var btnLeiaMais = document.getElementById("btnLeiaMais");
-
-    if (pontos.style.display === "none") {
-        pontos.style.display = "inline";
-        maisTexto.style.display = "none";
-        btnLeiaMais.innerHTML = "Leia Mais";
-    } else {
-        pontos.style.display = "none";
-        maisTexto.style.display = "inline";
-        btnLeiaMais.innerHTML = "Leia Menos";
-    }
-}
